3ff48f952ca78cb9f66953105faf5aec1a5ee30a
Sandeep Kumar <skuma143@asu.edu>
2020-03-22 23:39:13 -0700

    first commit

---
+++ solarpv/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
